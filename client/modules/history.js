0bf124e7701930212aec1820569ec82cbdf4b411
Nick Schmidt <nick@kreativgebiet.com>
2017-11-29 11:48:18 +0100

    track new page impression only after real change

--- client/modules/history.js
+++ client/modules/history.js
@@ -7,9 +7,10 @@ const history = createHistory();
  * Let the page jump to the top again after a history change
  */
 history.listen(() => {
-  setTimeout(() => window.scroll({ top: 0, left: 0, behavior: 'smooth' }), 500);
-
-  analytics.page();
+  setTimeout(() => {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    analytics.page();
+  }, 500);
 });
 
 export default history;
